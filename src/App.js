0cfde684bac7ecd95435c87743bfd06fe61c5c29
gauthmz <gauthambabu12@gmail.com>
2024-07-16 20:47:59 +0530

    Routing completed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import AddMovie from './components/AddMovie';
 import ViewAllMovies from './components/ViewAllMovies';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddMovie/>
-      <ViewAllMovies/>
-    </div>
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route path='/' element={<AddMovie/>}/>
+        <Route path='/view' element={<ViewAllMovies/>}/>
+
+
+
+
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
--- src/components/ViewAllMovies.jsx
+++ src/components/ViewAllMovies.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 const ViewAllMovies = () => {
     return (
         <div>
-
+            <NavBar />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -18,14 +19,14 @@ const ViewAllMovies = () => {
                                 </tr>
                             </thead>
                             <tbody>
-                                
+
                                 <tr>
                                     <th scope="row">Avengers</th>
                                     <td>Robert</td>
                                     <td>Scarlet</td>
                                     <td>Duffo</td>
                                 </tr>
-                               
+
                             </tbody>
                         </table>
 
